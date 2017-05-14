b6c6422dd0c1f7222c6f18f4a45222e6e06d2563
Aaron Jensen <aaronjensen@gmail.com>
2017-05-14 08:25:56 -0700

    Fix bad merge

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -175,7 +175,7 @@
              (org-agenda-files))))
 
 (setq org-refile-targets '((nil :maxlevel . 9)
-                           (org-agenda-files :maxlevel . 9)))
+                           (aj/refile-target-files :maxlevel . 9)))
 (setq org-refile-use-outline-path 'file)
 (setq org-outline-path-complete-in-steps nil)
 (setq org-refile-allow-creating-parent-nodes 'confirm)
